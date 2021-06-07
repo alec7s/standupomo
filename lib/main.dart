0ac4af4534ada2bc408f945c81e64524c47d779b
alec7s <alex.c.evans1@gmail.com>
2021-06-06 20:35:41 -0400

    Initial

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:standupomo/screens/homepage.dart';
+
+void main() {
+  runApp(StanduPomoApp());
+}
+
+class StanduPomoApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'StandUpomo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomePage(title: 'StandUpomo'),
+    );
+  }
+}
